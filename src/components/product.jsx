ae9b323cd227bb978305628297faad99e016ba80
Amisha Ajwani <ajwaniamisha12@gmail.com>
2022-10-22 01:26:20 +0530

    Frontend without redirecting fronm product page to detailed page

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ProductThumb from "./ProductThumb";
-import displayData from "../data.json"
+import displayData from "../data.json";
+import Navbar from "./nanvbar";
 const Product = () => {
     // let productList = displayData;
     console.log(displayData);
@@ -11,12 +12,13 @@ console.log(productList);
 
     // console.log(productList);
     return (
+        <>
+            {/* nav starts */}
+            <Navbar />
+            {/* nav ends */}
         <div className=" 2xl:container 2xl:mx-auto">
-            <div className=" bg-gray-50 text-center lg:py-10 md:py-8 py-6">
-                <p className=" w-10/12 mx-auto md:w-full  font-semibold lg:text-4xl text-3xl lg:leading-9 md:leading-7 leading-9 text-center text-gray-800">Summer Collection Vol-1</p>
-            </div>
             <div className=" py-6 lg:px-20 md:px-6 px-4">
-                <p className=" font-normal text-sm leading-3 text-gray-600 ">Home / Shop by Category / Women</p>
+                <p className=" font-normal text-sm leading-3 text-gray-600 ">Home / Shop by Category / Desktop</p>
                 <hr className=" w-full bg-gray-200 my-6" />
 
                 <div className=" flex justify-between items-center">
@@ -28,7 +30,7 @@ console.log(productList);
                         </svg>
                         <p className=" font-normal text-base leading-4 text-gray-800">Filter</p>
                     </div>
-                    <p className=" cursor-pointer hover:underline duration-100 font-normal text-base leading-4 text-gray-600">Showing 18 products</p>
+                    {/* <p className=" cursor-pointer hover:underline duration-100 font-normal text-base leading-4 text-gray-600">Showing 18 products</p> */}
                 </div>
 
                 <div className=" grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-y-12 lg:gap-x-8 sm:gap-y-10 sm:gap-x-6 gap-y-6 lg:mt-12 mt-10">
@@ -122,6 +124,7 @@ console.log(productList);
                 </div>
             </div>
         </div>
+        </>
     );
 };
 
--- src/components/productdetail.jsx
+++ src/components/productdetail.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
-
+import bg1 from './food1.jpg';
+import bg2 from './food2.jpg';
+import Navbar from "./nanvbar";
 const ProductDetail = (props) => {
     const [show, setShow] = useState(false);
     const [show2, setShow2] = useState(false);
 
     return (
+        <>
+          {/* nav starts */}
+          <Navbar />
+            {/* nav ends */}
         <div className="md:flex items-start justify-center py-12 2xl:px-20 md:px-6 px-4">
             <div className="xl:w-2/6 lg:w-2/5 w-80 md:block hidden">
-                <img className="w-full" alt="img of a girl posing" src="https://i.ibb.co/QMdWfzX/component-image-one.png" />
-                <img className="mt-6 w-full" alt="img of a girl posing" src="https://i.ibb.co/qxkRXSq/component-image-two.png" />
+                <img className="w-full" alt="app image" src={bg1} />
+                <img className="mt-6 w-full" alt="app image" src={bg2} />
             </div>
             <div className="md:hidden">
-                <img className="w-full" alt="img of a girl posing" src="https://i.ibb.co/QMdWfzX/component-image-one.png" />
+                <img className="w-full" alt="app image" src={bg1} />
                 <div className="flex items-center justify-between mt-3 space-x-4 md:space-x-0">
                     <img alt="img-tag-one" className="md:w-48 md:h-48 w-full" src="https://i.ibb.co/cYDrVGh/Rectangle-245.png" />
                     <img alt="img-tag-one" className="md:w-48 md:h-48 w-full" src="https://i.ibb.co/f17NXrW/Rectangle-244.png" />
@@ -21,7 +27,7 @@ const ProductDetail = (props) => {
             </div>
             <div className="xl:w-2/5 md:w-1/2 lg:ml-8 md:ml-6 md:mt-0 mt-6">
                 <div className="border-b border-gray-200 pb-6">
-                    <p className="text-sm leading-none text-gray-600">Balenciaga Fall Collection</p>
+                    <p className="text-sm leading-none text-gray-600">Android+Desktop</p>
                     <h1
                         className="
 							lg:text-2xl
@@ -33,39 +39,10 @@ const ProductDetail = (props) => {
 							mt-2
 						"
                     >
-                        Balenciaga Signature Sweatshirt
+                      Food delivery System
                     </h1>
                 </div>
-                <div className="py-4 border-b border-gray-200 flex items-center justify-between">
-                    <p className="text-base leading-4 text-gray-800">Colours</p>
-                    <div className="flex items-center justify-center">
-                        <p className="text-sm leading-none text-gray-600">Smoke Blue with red accents</p>
-                        <div
-                            className="
-								w-6
-								h-6
-								bg-gradient-to-b
-								from-gray-900
-								to-indigo-500
-								ml-3
-								mr-4
-								cursor-pointer
-							"
-                        ></div>
-                        <svg className="cursor-pointer" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L5 5L1 9" stroke="#4B5563" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
-                    </div>
-                </div>
-                <div className="py-4 border-b border-gray-200 flex items-center justify-between">
-                    <p className="text-base leading-4 text-gray-800">Size</p>
-                    <div className="flex items-center justify-center">
-                        <p className="text-sm leading-none text-gray-600 mr-3">38.2</p>
-                        <svg className="cursor-pointer" width="6" height="10" viewBox="0 0 6 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 1L5 5L1 9" stroke="#4B5563" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
-                        </svg>
-                    </div>
-                </div>
+               
                 <button
                     className="
 						focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800
@@ -87,20 +64,18 @@ const ProductDetail = (props) => {
                         <path d="M13.69 13.8567C14.1563 13.3905 14.4738 12.7966 14.6025 12.15C14.7312 11.5033 14.6653 10.8331 14.413 10.2239C14.1608 9.61476 13.7335 9.09411 13.1853 8.72779C12.6372 8.36148 11.9926 8.16595 11.3333 8.16595C10.674 8.16595 10.0295 8.36148 9.48133 8.72779C8.93314 9.09411 8.5059 9.61476 8.25364 10.2239C8.00138 10.8331 7.93543 11.5033 8.06412 12.15C8.19282 12.7966 8.51039 13.3905 8.97667 13.8567L11.3333 16.2142L13.69 13.8567Z" stroke="white" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                         <path d="M11.333 11.5V11.5083" stroke="white" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
-                    Check availability in store
+                    Contact Developer
                 </button>
                 <div>
                     <p className="xl:pr-48 text-base lg:leading-tight leading-normal text-gray-600 mt-7">It is a long established fact that a reader will be distracted by thereadable content of a page when looking at its layout. The point of usingLorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-                    <p className="text-base leading-4 mt-7 text-gray-600">Product Code: 8BN321AF2IF0NYA</p>
-                    <p className="text-base leading-4 mt-4 text-gray-600">Length: 13.2 inches</p>
-                    <p className="text-base leading-4 mt-4 text-gray-600">Height: 10 inches</p>
-                    <p className="text-base leading-4 mt-4 text-gray-600">Depth: 5.1 inches</p>
-                    <p className="md:w-96 text-base leading-normal text-gray-600 mt-4">Composition: 100% calf leather, inside: 100% lamb leather</p>
+                    <p className="text-base leading-4 mt-7 text-gray-600">Product Code: 8BN321AF2IF0NYA </p>
+                    <p className="text-base leading-4 mt-4 text-gray-600">Supported Platforms: Android, Desktop</p>
+
                 </div>
                 <div>
                     <div className="border-t border-b py-4 mt-7 border-gray-200">
                         <div onClick={() => setShow(!show)} className="flex justify-between items-center cursor-pointer">
-                            <p className="text-base leading-4 text-gray-800">Shipping and returns</p>
+                            <p className="text-base leading-4 text-gray-800">Contact us</p>
                             <button
                                 className="
 									cursor-pointer
@@ -115,11 +90,11 @@ const ProductDetail = (props) => {
                             </button>
                         </div>
                         <div className={"pt-4 text-base leading-normal pr-12 mt-4 text-gray-600 " + (show ? "block" : "hidden")} id="sect">
-                            You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are nonrefundable
+                        If you have any questions, contact us.
                         </div>
                     </div>
                 </div>
-                <div>
+                {/* <div>
                     <div className="border-b py-4 border-gray-200">
                         <div onClick={() => setShow2(!show2)} className="flex justify-between items-center cursor-pointer">
                             <p className="text-base leading-4 text-gray-800">Contact us</p>
@@ -137,12 +112,13 @@ const ProductDetail = (props) => {
                             </button>
                         </div>
                         <div className={"pt-4 text-base leading-normal pr-12 mt-4 text-gray-600 " + (show2 ? "block" : "hidden")} id="sect">
-                            If you have any questions on how to return your item to us, contact us.
+                            If you have any questions, contact us.
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
+        </>
     );
 };
 
